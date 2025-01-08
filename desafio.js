30e1f7204172ac5c4876180c1a63d1cf6e619229
00DouglasXavier <douglas.cred1@gmail.com>
2025-01-08 01:52:40 -0300

    Primeiro commit

---
+++ desafio.js
@@ -0,0 +1,25 @@
+// Desafio classificar de nivel = herói
+
+let nome = "Aragorn"
+let xp = 6000
+let nivel 
+
+if (xp <=1000)
+    nivel = ("ferro")
+    
+else if (xp >= 1011 && xp <=2000)
+    nivel = ("Bronze")
+else if (xp >= 2001 && xp <=5000)
+    nivel = (Prata)
+else if (xp >=5001 && xp <=7000)
+    nivel = ("Ouro")
+else if (xp >= 7001 && xp <= 8000)
+    nivel = (Platina)
+else if (xp >=8001 && xp<=9000)
+    nivel = ("Ascendente")
+else if (xp >= 9001 && xp <=10000)
+    nivel = ("Imortal")
+else 
+    nivel = ("Radiante")
+
+    console.log ("O heroi de nome: " + nome + "está no nivel  : " + nivel)
